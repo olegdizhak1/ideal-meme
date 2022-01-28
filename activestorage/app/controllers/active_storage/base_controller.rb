a15816b9fb95c61f4730eabea0c892ebf669fe78
Oleg <79935602+olegdizhak1@users.noreply.github.com>
2022-01-28 05:06:11 +0200

    Initialize fix-as-with-api-apps

---
+++ activestorage/app/controllers/active_storage/base_controller.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# The base class for all Active Storage controllers.
+class ActiveStorage::BaseController < ActionController::API
+  include ActionController::RequestForgeryProtection
+  include ActiveStorage::SetCurrent
+
+  protect_from_forgery with: :exception, if: :protect_against_forgery?
+
+  private
+    def stream(blob)
+      blob.download do |chunk|
+        response.stream.write chunk
+      end
+    ensure
+      response.stream.close
+    end
+
+  public
+
+  ActiveSupport.run_load_hooks :active_storage_base_controller, self
+end
