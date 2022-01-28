528129b951e2b39b5cf14a87d5ede0e6bd30ad2b
Oleg <79935602+olegdizhak1@users.noreply.github.com>
2022-01-28 05:02:50 +0200

    Initialize freeze-configuration_hash

---
+++ railties/test/application/bin_setup_test.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+require "isolation/abstract_unit"
+
+module ApplicationTests
+  class BinSetupTest < ActiveSupport::TestCase
+    include ActiveSupport::Testing::Isolation
+
+    setup :build_app
+    teardown :teardown_app
+
+    def test_bin_setup
+      Dir.chdir(app_path) do
+        rails "generate", "model", "article"
+
+        list_tables = lambda { rails("runner", "p ActiveRecord::Base.connection.tables").strip }
+        File.write("log/test.log", "zomg!")
+
+        assert_match "[]", list_tables.call
+        assert_equal 5, File.size("log/test.log")
+        assert_not File.exist?("tmp/restart.txt")
+
+        `bin/setup 2>&1`
+        assert_equal 0, File.size("log/test.log")
+        assert_match '["schema_migrations", "ar_internal_metadata", "articles"]', list_tables.call
+        assert File.exist?("tmp/restart.txt")
+      end
+    end
+
+    def test_bin_setup_output
+      Dir.chdir(app_path) do
+        # SQLite3 seems to auto-create the database on first checkout.
+        rails "db:system:change", "--to=postgresql"
+        rails "db:drop", allow_failure: true
+
+        app_file "db/schema.rb", ""
+
+        output = `bin/setup 2>&1`
+
+        # Ignore line that's only output by Bundler < 1.14
+        output.sub!(/^Resolving dependencies\.\.\.\n/, "")
+        # Suppress Bundler platform warnings from output
+        output.gsub!(/^The dependency .* will be unused .*\.\n/, "")
+        # Ignore warnings such as `Psych.safe_load is deprecated`
+        output.gsub!(/^warning:\s.*\n/, "")
+
+        assert_match(<<~OUTPUT, output)
+          == Installing dependencies ==
+          The Gemfile's dependencies are satisfied
+
+          == Preparing database ==
+        OUTPUT
+
+        assert_match("Created database 'app_development'", output)
+        assert_match("Created database 'app_test'", output)
+        assert_match("== Removing old logs and tempfiles ==", output)
+        assert_match("== Restarting application server ==", output)
+      end
+    end
+  end
+end
