1d14b7421a9078ac0fd6ed9d2b129214c7e57f92
Oleg <79935602+olegdizhak1@users.noreply.github.com>
2022-01-28 05:06:31 +0200

    Initialize 4-2-stable

---
+++ actionpack/test/controller/new_base/content_negotiation_test.rb
@@ -0,0 +1,37 @@
+require 'abstract_unit'
+
+module ContentNegotiation
+
+  # This has no layout and it works
+  class BasicController < ActionController::Base
+    self.view_paths = [ActionView::FixtureResolver.new(
+      "content_negotiation/basic/hello.html.erb" => "Hello world <%= request.formats.first.to_s %>!"
+    )]
+
+    def all
+      render :text => self.formats.inspect
+    end
+  end
+
+  class TestContentNegotiation < Rack::TestCase
+    test "A */* Accept header will return HTML" do
+      get "/content_negotiation/basic/hello", {}, "HTTP_ACCEPT" => "*/*"
+      assert_body "Hello world */*!"
+    end
+
+    test "A js or */* Accept header will return HTML" do
+      get "/content_negotiation/basic/hello", {}, { "HTTP_ACCEPT" => "text/javascript, */*" }
+      assert_body "Hello world text/html!"
+    end
+
+    test "A js or */* Accept header on xhr will return HTML" do
+      xhr :get, "/content_negotiation/basic/hello", {}, { "HTTP_ACCEPT" => "text/javascript, */*" }
+      assert_body "Hello world text/javascript!"
+    end
+
+    test "Unregistered mimes are ignored" do
+      get "/content_negotiation/basic/all", {}, { "HTTP_ACCEPT" => "text/plain, mime/another" }
+      assert_body '[:text]'
+    end
+  end
+end
